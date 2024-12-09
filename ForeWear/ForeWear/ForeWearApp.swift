b3fd64490c26748479e9c86a843f6b53f36592a5
Valeria Guardascione <va.guardascione362@gmail.com>
2024-12-09 10:25:48 +0100

    First commit

---
+++ ForeWear/ForeWear/ForeWearApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForeWearApp.swift
+//  ForeWear
+//
+//  Created by Valeria Guardascione on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ForeWearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
