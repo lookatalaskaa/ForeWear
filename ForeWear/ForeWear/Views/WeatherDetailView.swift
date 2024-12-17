691e4123aff39e443b71799760da6a1894d3697c
Valeria Guardascione <va.guardascione362@gmail.com>
2024-12-17 19:02:04 +0100

    Added a sheet to show weather details + avatar animation

---
+++ ForeWear/ForeWear/Views/WeatherDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherDetailView.swift
+//  ForeWear
+//
+//  Created by Valeria Guardascione on 15/12/24.
+//
+
+import SwiftUI
+
+struct WeatherDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherDetailView()
+}
